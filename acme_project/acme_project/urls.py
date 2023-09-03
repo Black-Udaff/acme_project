48e108d64692bff2bfb249ad7722012b8a1491b6
Alexey Kazemirenko <xxxalex_93@icloud.com>
2023-09-03 21:42:12 +0300

    1232

--- acme_project/acme_project/urls.py
+++ acme_project/acme_project/urls.py
@@ -4,7 +4,8 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
+    path('auth/', include('django.contrib.auth.urls')),
     path('', include('pages.urls')),
     path('admin/', admin.site.urls),
     path('birthday/', include('birthday.urls')),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) 
+] 
--- acme_project/birthday/views.py
+++ acme_project/birthday/views.py
@@ -1,22 +1,23 @@
-from django.shortcuts import render, get_object_or_404, redirect
-
 from .form import BirthdayForm
 from .utils import calculate_birthday_countdown
 from .models import Birthday
-from django.core.paginator import Paginator
-from django.views.generic import ListView, CreateView, UpdateView, DeleteView
+
+from django.views.generic import (
+    ListView,
+    CreateView,
+    UpdateView,
+    DeleteView,
+    DetailView,
+)
 from django.urls import reverse_lazy
 
 
 class BirthdayMixin:
     model = Birthday
-    form_class = BirthdayForm
-    template_name = 'birthday/birthday.html'
-    success_url = reverse_lazy('birthday:list')
 
 
 class BirthdayCreateView(BirthdayMixin, CreateView):
-    pass
+    form_class = BirthdayForm
 
 
 class BirthdayListView(ListView):
@@ -25,11 +26,25 @@ class BirthdayListView(ListView):
     paginate_by = 10
 
 
-class BirthdayDeleteView(DeleteView):
+class BirthdayDeleteView(BirthdayMixin, DeleteView):
     model = Birthday
-    template_name = 'birthday/birthday.html'
     success_url = reverse_lazy('birthday:list')
 
 
 class BirthdayUpdateView(BirthdayMixin, UpdateView):
-    pass
+    form_class = BirthdayForm
+
+
+class BirthdayDetailView(DetailView):
+    model = Birthday
+
+    def get_context_data(self, **kwargs):
+        # Получаем словарь контекста:
+        context = super().get_context_data(**kwargs)
+        # Добавляем в словарь новый ключ:
+        context['birthday_countdown'] = calculate_birthday_countdown(
+            # Дату рождения берём из объекта в словаре context:
+            self.object.birthday
+        )
+        # Возвращаем словарь контекста.
+        return context
